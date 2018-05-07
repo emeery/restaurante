6d1134c64ab09833e9018ba5c1a48a7645088411
emeery <gerardoemery@live.com.mx>
2018-05-07 12:43:08 -0500

    una ruta

--- src/componentes/PaginaNoEncontrada.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-
-// const PaginaNoEncontrada = () => (
-//     <div>
-//         <h2>404 - UPS!</h2>
-//         <h3>No es lo que buscas?</h3>
-//     </div>
-// ); export default PaginaNoEncontrada;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,11 +2,8 @@ import React from 'react';
 import {
 BrowserRouter, Switch, Route, Link, NavLink} from 'react-router-dom';
 import Cabecera from '../componentes/Cabecera';
-import Contacto from '../componentes/Contacto';
 import AppUsuarios from '../componentes/AppUsuarios';
-import PaginaNoEncontrada from '../componentes/PaginaNoEncontrada';
-import Portafolio from '../componentes/Portafolio';
-import PortafolioItem from '../componentes/PortafolioItem' 
+
 
 
 const AppRouter = () => (
@@ -25,7 +22,3 @@ const AppRouter = () => (
 ); 
 export default AppRouter;
 
-// <Route path="/portafolio" component={Portafolio} exact={true} />
-//             <Route path="/portafolio/:id" component={PortafolioItem}/>
-//             <Route path="/contacto" component={Contacto} />
-//             <Route component={PaginaNoEncontrada} />
